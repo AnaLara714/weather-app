a96c004f7198d9af682eb099b56d401f664b0bb1
AnaLara714 <Analara714@gmail.com>
2023-04-27 20:13:22 -0300

    correcao da responsividade para telas de 1280px

--- components/main/main.tsx
+++ components/main/main.tsx
@@ -18,7 +18,7 @@ export default function Main() {
         : infosCity?.current.is_day === 1 
         ? "bg-[#fbbf24] " 
         : "bg-[#155e75]"
-    } max-sm:h-max p-4 h-screen max-lg:h-max flex flex-col `}
+    } max-sm:h-max p-4 h-screen lg:h-max xl:h-screen flex flex-col `}
     >
       <ToastContainer
         position="top-center" autoClose={2000} hideProgressBar={false} newestOnTop={false}
